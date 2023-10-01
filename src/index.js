073b1a69bbbbe4ec8b649c509f0ab4cb3107c1cb
ABHIJEET <abhijeetsahoomsd@gmail.com>
2023-10-01 12:27:12 +0530

    added bookings model and also added enums to the utils folder

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);

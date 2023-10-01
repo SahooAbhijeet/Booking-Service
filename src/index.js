d2a4e82d0a3e0edc584554afd6c1bf276fae2154
ABHIJEET <abhijeetsahoomsd@gmail.com>
2023-10-01 10:28:38 +0530

    basic setup done

--- src/index.js
+++ src/index.js
@@ -8,9 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use('/api', apiRoutes);
+// app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-    const { City, Airport } = require('./models');
 });

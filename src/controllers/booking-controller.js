2037cf31293ba35d2eda6d1538f3310eea0f7499
ABHIJEET <abhijeetsahoomsd@gmail.com>
2023-10-20 17:54:55 +0530

    started booking implemntation

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,3 +1,27 @@
+const {BookingService} = require('../services');
+const {StatusCodes} = require('http-status-codes');
+const { SuccessResponse, ErrorResponse } = require('../utils/common');
+
+async function createBooking(req, res) {
+    try {
+        const response = await BookingService.createBooking({
+            flightId: req.body.flightId,
+            userId: req.body.userId,
+            noofSeats: req.body.noofSeats
+        });
+        SuccessResponse.data = response;
+        return res
+                .status(StatusCodes.OK)
+                .json(SuccessResponse);
+    } catch(error) {
+        ErrorResponse.error = error;
+        return res
+                .status(error.statusCode)
+                .json(ErrorResponse);
+    }
+}
+
+
 module.exports = {
-    
+    createBooking
 }

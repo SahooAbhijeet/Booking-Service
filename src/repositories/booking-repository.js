7725ea1992af9db926a7d4807a044429e940295b
ABHIJEET <abhijeetsahoomsd@gmail.com>
2023-10-21 08:07:02 +0530

    added booking logic with transaction

--- src/repositories/booking-repository.js
+++ src/repositories/booking-repository.js
@@ -1,12 +1,15 @@
-const { StatusCodes } = require('http-status-codes');
 const CrudRepository = require('./crud-repository');
 const { Booking } = require('../models');
 
 class BookingRepository extends CrudRepository {
     constructor() {
         super(Booking);
+    }                     
+
+    async  createBooking(data, transaction) {
+        const response = await Booking.create(data, {transaction: transaction});
+        return response;
     }
 }
-module.exports = {
-    BookingRepository
-}
+
+module.exports = BookingRepository
